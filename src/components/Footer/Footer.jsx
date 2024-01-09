2d620658497a8d9358abf3c33aa33a39c579dff5
rcuthbertson25 <ryancuth25@yahoo.com>
2024-01-09 12:51:25 -0500

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,16 +3,37 @@ import './Footer.css'
 import Github from "../../assets/github.png"
 import Instagram from "../../assets/instagram.png"
 import LinkedIn from "../../assets/linkedin.png"
+import {Navigate} from 'react-router-dom'
 
 const Footer = () => {
+
+  const redirectToGithub = () => {
+    const githubProfileURL = 'https://github.com/rcuthbertson25';
+    window.open(githubProfileURL, '_blank');
+  };
+  const redirectToLinkedIn = () => {
+    const linkedinProfileURL = 'https://www.linkedin.com/in/ryan-cuthbertson-61a980281';
+    window.open(linkedinProfileURL, '_blank');
+  };
+
   return (
     <div className="Footer-container">
         <hr />
         <div className="footer">
             <div className="social-links">
-                <img src={Github} alt="" />
+              <img
+                src={Github}
+                alt=""
+                onClick={redirectToGithub}
+                style={{ cursor: 'pointer' }}
+              />
                 <img src={Instagram} alt="" />
-                <img src={LinkedIn} alt="" />
+              <img
+                src={LinkedIn}
+                alt=""
+                onClick={redirectToLinkedIn}
+                style={{ cursor: 'pointer' }}
+              />
             </div>
             <div className="logo">
               <span>Flex</span>
